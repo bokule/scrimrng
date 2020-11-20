09d5255fbbd35cd937403667c13d8483204c047e
bokule <58610688+bokule@users.noreply.github.com>
2020-11-20 15:35:21 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,143 @@
+function start() {
+    btnAddName.addEventListener("click", addName);
+    tbName.addEventListener("keydown", function(event) {
+        if(event.keyCode === 13) {
+            event.preventDefault();
+            addName();
+        }
+    });
+    btnBack.addEventListener("click", reverseSubmit);
+    //document.querySelectorAll(".btnRemove").addEventListener("click", removeName);
+}
+
+var name;
+var namesArray = [];
+//var namesArray = ['a','b','c','d','e','f','g','h','i','j'];
+
+function addName() {
+    if(tbName.value != "" && namesArray.length < 10) {
+        name = tbName.value.toUpperCase();
+        if(isNotDuplicate(name, namesArray)){
+            namesArray.push(name);
+            tbName.value = '';
+        }
+    }
+    printNames();
+    updateNumber();
+    enableSubmit();
+}
+
+function printNames() {
+    namesList.innerHTML = '';
+    for(index in namesArray) {
+        var liTag = document.createElement("li");
+        //var liContent = document.createTextNode(namesArray[index]);
+        //liTag.appendChild(liContent);
+        liTag.innerHTML += `<span class="name">${namesArray[index]}</span><button class="btnRemove" id="btnRemove${index}" onClick="removeName(${index});">X</button>`;
+        namesList.appendChild(liTag);
+    }
+}
+
+function isNotDuplicate(element, arr) {
+    for(index of arr){
+        if(index == element){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+function updateNumber() {
+    numberOfNames.textContent = `${namesArray.length}/10`;
+}
+
+function enableSubmit() {
+    if(namesArray.length == 10) {
+        submitButton.addEventListener("click", submit);
+    } else {
+        submitButton.removeEventListener("click", submit);
+    }
+}
+
+function submit() {
+    firstStep.style.display = "none";
+    secondStep.style.display = "block";
+    createTeam1();
+    createTeam2();
+    printTeam1();
+    printTeam2();
+    btnNext.style.display = "none";
+}
+
+function reverseSubmit() {
+    firstStep.style.display = "block";
+    secondStep.style.display = "none";
+    btnNext.style.display = "block";
+}
+
+function next() {
+    firstStep.style.display = "none";
+    secondStep.style.display = "block";
+    btnNext.style.display = "none";
+}
+
+var team1 = [];
+var team2 = [];
+var roles = ['TOP', 'JUNGLE', 'MID', 'ADC', 'SUPPORT'];
+
+
+function createTeam1() {
+    team1 = [];
+    for(let i = 0; i < namesArray.length/2; i++) {
+        var randomName = namesArray[rng()];
+        if(isNotDuplicate(randomName, team1)) {
+            team1.push(randomName);
+        } else i--;
+    }
+}
+
+function createTeam2() {
+    team2 = [];
+    for(let i = 0; i < namesArray.length/2; i++) {
+        var randomName = namesArray[rng()];
+        if(isNotDuplicate(randomName, team1) && isNotDuplicate(randomName, team2)) {
+            team2.push(randomName);
+        } else i--;
+    }
+}
+
+function rng() {
+    return Math.floor(Math.random() * 10);
+}
+
+function printTeam1() {
+    team1List.innerHTML = "";
+    for(index in team1) {
+        var teamMember = document.createElement("li");
+        //var teamMemberName = document.createTextNode(`${team1[index]} - ${roles[index]}`);
+        //teamMember.appendChild(teamMemberName);
+        teamMember.innerHTML += `<span class="name">${team1[index]}</span><span>${roles[index]}</span>`;
+        team1List.appendChild(teamMember);
+    }
+}
+
+function printTeam2() {
+    team2List.innerHTML = "";
+    for(index in team2) {
+        var teamMember = document.createElement("li");
+        //var teamMemberName = document.createTextNode(`${team2[index]} - ${roles[index]}`);
+        //teamMember.appendChild(teamMemberName);
+        teamMember.innerHTML += `<span class="name">${team2[index]}</span><span>${roles[index]}</span>`;
+        team2List.appendChild(teamMember);
+    }
+}
+
+function removeName(index) {
+    namesArray.splice(index, 1);
+    printNames();
+    updateNumber();
+}
+
+// EXECUTION
+
+start();
